96a527e25bd6ce360ebfda441c9030a5cb665ef8
VolodymyrKoch <volodymyrkocherhan@gmail.com>
2020-11-23 23:31:23 +0200

    fixed error in render

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ export default class App extends Component {
     neutral: 0,
     bad: 0,
   };
-  handlSetState = (event) => {
-    this.setState((prevState) => ({
-      [event.target.name]: prevState[event.target.name]+1,
+  handlSetState = event => {
+    this.setState(prevState => ({
+      [event.target.name]: prevState[event.target.name] + 1,
     }));
   };
 
-  return() {
-    <>
+  render() {
+    return (
+      // <>
       <Feedback
         good={this.state.good}
         neutral={this.state.neutral}
-        bad={this.stete.bad}
+        bad={this.state.bad}
         propSet={this.handlSetState}
       />
-    </>;
+      // </>
+    );
   }
 }
-
-
